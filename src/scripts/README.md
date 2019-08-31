80e8b84ba76a392e63acb8dadaf9866d33dcffcc
Juan Latorre <juanlatorre@protonmail.com>
2019-08-30 22:17:12 -0400

    new: added scripts to copy data from gamepress

---
+++ src/scripts/README.md
@@ -0,0 +1,6 @@
+# Scripts | Pokemon Masters DB - Game Database
+
+In this section there are some scripts that I wrote to copy the data from pokemonmasters.gamepress.gg.
+I made this because I'm no masochist, when I faced the fact that everthing must be copied by hand, line by line I tryed to avoid that before suicide.
+
+Feel free to use the scripts with the same purpose or modify them to your will.
---
+++ src/scripts/getTrainerData.js
@@ -0,0 +1,61 @@
+javascript: var name;
+var info;
+var base_potential;
+var recruit_method;
+var image;
+var sync_pair_story = [];
+var pokemon_list = [];
+name = document.querySelector("#page-title").querySelector("h1").innerHTML;
+info = document.querySelector(".trainer-description").innerHTML;
+var base_potential_src = document
+  .querySelector(".base-potential-image")
+  .querySelector("img").src;
+if (base_potential_src.includes("5-star")) {
+  base_potential = 5;
+} else if (base_potential_src.includes("4-star")) {
+  base_potential = 4;
+} else if (base_potential_src.includes("3-star")) {
+  base_potential = 3;
+}
+var allTh = document.querySelectorAll("th");
+allTh.forEach(function(th) {
+  if (th.innerHTML === "Recruit Method") {
+    if (th.nextSibling.innerHTML === undefined) {
+      recruit_method = "";
+    } else {
+      recruit_method = th.nextSibling.innerHTML;
+    }
+  }
+});
+image = document
+  .querySelector(".trainer-top-image")
+  .querySelector("a")
+  .href.split("/")
+  .pop();
+var sync_pair_story_title = document.querySelectorAll(".taxonomy-title");
+sync_pair_story_title.forEach(function(node) {
+  sync_pair_story.push(node.querySelector("a").innerHTML);
+});
+var pokemon_trainer_node_pokemon_title = document.querySelectorAll(
+  ".pokemon-trainer-node-pokemon-title"
+);
+pokemon_trainer_node_pokemon_title.forEach(function(node) {
+  pokemon_list.push(node.innerHTML.split("amp;").pop());
+});
+var json =
+  '{"name":"' +
+  name +
+  '","info":"' +
+  info +
+  '","base_potential":"' +
+  base_potential +
+  '","recruit_method":"' +
+  recruit_method +
+  '","pokemon_list":"' +
+  pokemon_list +
+  '","image":"' +
+  image +
+  '","sync_pair_story":"' +
+  sync_pair_story +
+  '"}';
+alert(json);
