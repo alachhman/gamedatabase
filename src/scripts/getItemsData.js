d1c8fc5ca52565cd04d5319f8af0453dee334851
Juan Latorre <juanlatorre@protonmail.com>
2019-09-03 11:34:54 -0400

    clean all bad items json

--- src/scripts/getItemsData.js
+++ src/scripts/getItemsData.js
@@ -19,35 +19,19 @@ image = document
 
 image = decodeURIComponent(image);
 
-var imagencita = document
-  .querySelector(".item-rarity-icon")
-  .querySelector("a")
-  .querySelector("img").src;
-
 description = document
   .querySelectorAll(".description-cell")[0]
   .querySelector("p").innerHTML;
 
-fetch(imagencita)
-  .then(response => response.blob())
-  .then(blob => {
-    var reader = new FileReader();
-    reader.readAsDataURL(blob);
-    reader.onloadend = () => {
-      var base64data = reader.result;
-
-      var json = {
-        name: name,
-        image: base64data,
-        description: description
-      };
-      var dataStr =
-        "data:text/json;charset=utf-8," +
-        encodeURIComponent(JSON.stringify(json));
-      var dummyAnchor = document.createElement("a");
-      document.querySelector("body").appendChild(dummyAnchor);
-      dummyAnchor.setAttribute("href", dataStr);
-      dummyAnchor.setAttribute("download", filename + ".json");
-      dummyAnchor.click();
-    };
-  });
+var json = {
+  name: name,
+  image: image,
+  description: description
+};
+var dataStr =
+  "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json));
+var dummyAnchor = document.createElement("a");
+document.querySelector("body").appendChild(dummyAnchor);
+dummyAnchor.setAttribute("href", dataStr);
+dummyAnchor.setAttribute("download", filename + ".json");
+dummyAnchor.click();
