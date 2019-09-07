964c1112065303ac01e5a012fc6e891141e54dc3
Juan Latorre <juanlatorre@protonmail.com>
2019-09-07 12:38:28 -0400

    remade get pokemon script

--- src/scripts/getPokemonData.js
+++ src/scripts/getPokemonData.js
@@ -1,117 +1,173 @@
-const https = require("https");
 const fs = require("fs");
+const cheerio = require("cheerio");
+const axios = require("axios");
+log = console.log;
 
-function getAndWritePokemonInfo() {
-  https
-    .get(
-      "https://gamepress.gg/sites/default/files/aggregatedjson/PokemonMastersPokemonList.json",
-      resp => {
-        let data = "";
+const getPokemonMastersPokemonList = async () => {
+  let json = await axios.get(
+    "https://gamepress.gg/sites/default/files/aggregatedjson/PokemonMastersPokemonList.json"
+  );
 
-        // A chunk of data has been recieved.
-        resp.on("data", chunk => {
-          data += chunk;
+  for (const pokemon of json.data) {
+    let name = pokemon.title_plain
+      .split("&amp;")
+      .pop()
+      .trim();
+    let filename = name
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/\(|\)/g, "");
+
+    let type1 = pokemon.type1_plain;
+    let type2 = pokemon.type2_plain;
+    let weakness = pokemon.weakness_plain;
+    let role = pokemon.role;
+    let image = pokemon.icon
+      .replace(/\n/g, "")
+      .trim()
+      .replace(/<a href="(.*)"><img src="|".*$/gm, "")
+      .replace(/^\/sites.*(?=pm)|\?.*$/g, "");
+    let moves = [];
+
+    const pokemonMovesUri = await axios.get(
+      "https://pokemonmasters.gamepress.gg" + pokemon.path
+    );
+
+    const $ = cheerio.load(pokemonMovesUri.data);
+
+    const movesTable = $(
+      ".view-moves-on-pokemon-node.pokemon-node-moves-container > div.views-row.pokemon-node-move > div.move-pokemon-page-container"
+    );
+
+    movesTable.each(function() {
+      const move_name = $(this)
+        .find(".move-pokemon-page-title > a")
+        .text();
+      const move_type = $(this)
+        .find(".move-type > td")
+        .text()
+        .trim();
+      const move_category = $(this)
+        .find(".move-category > td > img")
+        .attr("src")
+        .split("/")
+        .pop()
+        .split(".")[0]
+        .replace(/%20/g, " ");
+      const move_power = {
+        min_power: parseInt(
+          $(this)
+            .find(".move-pokemon-page-power > span.min-power")
+            .text()
+        ),
+        max_power: parseInt(
+          $(this)
+            .find(".move-pokemon-page-power > span.max-power")
+            .text()
+        )
+      };
+      const move_accuracy = parseInt(
+        $(this)
+          .find(".move-accuracy > th:contains('Accuracy') + td")
+          .text()
+      );
+      const move_target = $(this)
+        .find(".move-target > th:contains('Target') + td")
+        .text();
+      const move_cost =
+        $(this).find(".pokemon-cost-box-PG").length > 0
+          ? parseInt($(this).find(".pokemon-cost-box-PG").length)
+          : "";
+      const move_uses =
+        $(this).find(".move-uses > td > a").length > 0
+          ? parseInt(
+              $(this)
+                .find(".move-uses > td > a")
+                .text()
+            )
+          : "";
+      const move_effect = $(this)
+        .find(".move-effect")
+        .text();
+      const move_unlock_requirements = [];
+
+      $(this)
+        .find(
+          ".field--name-field-move-unlock-requirements > .field__items > .field__item"
+        )
+        .each(function() {
+          move_unlock_requirements.push(
+            $(this)
+              .find(".move-unlock-requirements-text")
+              .text()
+          );
         });
 
-        // The whole response has been received. Print out the result.
-        resp.on("end", () => {
-          let json = JSON.parse(data);
-          json.forEach(pokemon => {
-            name = pokemon.title_plain
-              .split("&amp;")
-              .pop()
-              .trim();
-            filename = name
-              .toLowerCase()
-              .replace(/ /g, "-")
-              .replace(/\(|\)/g, "");
-            type1 = pokemon.type1_plain;
-            type2 = pokemon.type2_plain;
-            weakness = pokemon.weakness_plain;
-            role = pokemon.role;
-            image = pokemon.icon
-              .replace(/\n/g, "")
-              .trim()
-              .replace(/<a href="(.*)"><img src="|".*$/gm, "")
-              .replace(/^\/sites.*(?=pm)|\?.*$/g, "");
-
-            let object = {
-              name: name === "" ? "" : name,
-              type1: type1 === "" ? "" : type1,
-              type2: type2 === "" ? "" : type2,
-              weakness: weakness === "" ? "" : weakness,
-              role: role === "" ? "" : role,
-              image: image === "" ? "" : image,
-              stats: {}
-            };
-
-            let objectString = JSON.stringify(object);
-            fs.writeFileSync(filename + ".json", objectString);
-          });
-        });
-      }
-    )
-    .on("error", err => {
-      console.log("Error: " + err.message);
+      moves.push({
+        name: move_name,
+        type: move_type,
+        category: move_category,
+        power: move_power,
+        accuracy: move_accuracy,
+        target: move_target,
+        cost: move_cost,
+        uses: parseInt(move_uses),
+        effect: move_effect,
+        unlock_requirements: move_unlock_requirements
+      });
     });
-}
 
-function getAndWritePokemonStats() {
-  https
-    .get(
-      "https://gamepress.gg/sites/default/files/aggregatedjson/StatRankingsMastersPokemon.json",
-      resp => {
-        let data = "";
+    let stats = [];
 
-        // A chunk of data has been recieved.
-        resp.on("data", chunk => {
-          data += chunk;
-        });
+    const pokemonStats = await axios.get(
+      "https://gamepress.gg/sites/default/files/aggregatedjson/StatRankingsMastersPokemon.json"
+    );
 
-        // The whole response has been received. Print out the result.
-        resp.on("end", () => {
-          let json = JSON.parse(data);
-          json.forEach(stat => {
-            filename = stat.title
-              .split("&amp;")
-              .pop()
-              .trim()
-              .toLowerCase()
-              .replace(/ /g, "-")
-              .replace(/\(|\)/g, "");
-
-            let rawdata = fs.readFileSync("../pokemon/" + filename + ".json");
-            let pokemon = JSON.parse(rawdata);
-            pokemon.stats = {
-              base: {
-                attack: parseInt(stat.field_base_attack),
-                defense: parseInt(stat.field_base_defense),
-                hp: parseInt(stat.field_base_hp),
-                speed: parseInt(stat.field_base_speed),
-                sp_atk: parseInt(stat.field_base_sp_atk),
-                sp_def: parseInt(stat.field_base_sp_def)
-              },
-              max: {
-                attack: parseInt(stat.field_max_attack),
-                bulk: parseInt(stat.field_max_bulk_floored),
-                defense: parseInt(stat.field_max_defense),
-                hp: parseInt(stat.field_max_hp),
-                speed: parseInt(stat.field_max_speed),
-                sp_atk: parseInt(stat.field_max_sp_atk),
-                sp_def: parseInt(stat.field_max_sp_def)
-              }
-            };
-
-            let objectString = JSON.stringify(pokemon);
-            fs.writeFileSync(filename + ".json", objectString);
-          });
-        });
+    await pokemonStats.data.map(stat => {
+      let statFileName = stat.title
+        .split("&amp;")
+        .pop()
+        .trim()
+        .toLowerCase()
+        .replace(/ /g, "-")
+        .replace(/\(|\)/g, "");
+      if (statFileName === filename) {
+        stats = {
+          base: {
+            attack: parseInt(stat.field_base_attack),
+            defense: parseInt(stat.field_base_defense),
+            hp: parseInt(stat.field_base_hp),
+            speed: parseInt(stat.field_base_speed),
+            sp_atk: parseInt(stat.field_base_sp_atk),
+            sp_def: parseInt(stat.field_base_sp_def)
+          },
+          max: {
+            attack: parseInt(stat.field_max_attack),
+            bulk: parseInt(stat.field_max_bulk_floored),
+            defense: parseInt(stat.field_max_defense),
+            hp: parseInt(stat.field_max_hp),
+            speed: parseInt(stat.field_max_speed),
+            sp_atk: parseInt(stat.field_max_sp_atk),
+            sp_def: parseInt(stat.field_max_sp_def)
+          }
+        };
       }
-    )
-    .on("error", err => {
-      console.log("Error: " + err.message);
     });
-}
 
-getAndWritePokemonInfo();
-getAndWritePokemonStats();
+    let object = {
+      name: name === "" ? "" : name,
+      type1: type1 === "" ? "" : type1,
+      type2: type2 === "" ? "" : type2,
+      weakness: weakness === "" ? "" : weakness,
+      role: role === "" ? "" : role,
+      image: image === "" ? "" : image,
+      stats: stats,
+      moves: moves
+    };
+
+    let objectString = JSON.stringify(object);
+    fs.writeFileSync(filename + ".json", objectString);
+  }
+};
+
+getPokemonMastersPokemonList();
