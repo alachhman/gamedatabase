940a92b16f76c0e1981bc507d671b6557efcf639
Anthony <lachhman.anthony@gmail.com>
2019-12-04 18:19:17 -0500

    Updated dragalia scraper

--- src/scripts/dragscripts/getUnitData.js
+++ src/scripts/dragscripts/getUnitData.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 const axios = require("axios");
 log = console.log;
-const pokemonFolder = "D:\\Programming\\gamedatabase\\src\\pokemon\\";
 
 const getDragaliaUnits = async () => {
     let json = await axios.get(
@@ -17,6 +16,52 @@ const getDragaliaUnits = async () => {
         const image = "https://gamepress.gg" + $('#char-image > a > img').attr('src');
         const description = $('#stats-table > tbody > tr:nth-child(2) > td > p').text();
         const title = $('#overview-table > tbody > tr:nth-child(1) > th').text();
+
+        const cvTemp = $('#stats-table > tbody > tr:nth-child(4)').text().split("\n");
+        const cv = [cvTemp[1], cvTemp[2]];
+
+        const abilities = [];
+        $('.view.view-ability-on-character-page > div > div > div > span > table > tbody').each((i, skillTable) => {
+            const abilitiesTemp = [];
+            $(skillTable).find('.ability-info').each((i, skill) => {
+                abilitiesTemp.push(
+                    {
+                        name: $(skill).find('> b > a').text(),
+                        effect: $(skill).find('> .ability-effect').text().replace("\n", ""),
+                    }
+                )
+            });
+            abilities.push(abilitiesTemp);
+        });
+
+        const skills = [];
+        $('.view.view-skills-on-character > div > div > div > span').each((i, skill) => {
+            const name = $(skill).find('.skill-main-title > td > b > a').text();
+            const cost = $(skill).find('.skill-extra-data > tbody > tr > td:nth-child(2)').text();
+            const iframe = $(skill).find('.skill-extra-data > tbody > tr > td:nth-child(4)').text();
+            const levels = [];
+            $(skill).find('table').each((i, table) => {
+                if (i !== 1) {
+                    $(table).find('> tbody > tr').each((i, level) => {
+                        if (i !== 0) {
+                            levels.push(
+                                {
+                                    level: $(level).find('> th').text(),
+                                    description: $(level).find('> td:nth-child(2)').text(),
+                                }
+                            );
+                        }
+                    });
+                }
+            });
+            skills.push({
+                name: name,
+                levels: levels,
+                cost: cost,
+                iframe: iframe
+            });
+        });
+
         const rarity = unit.rarity_num;
         const element = unit.char_element;
         const weapon = unit.weapon_type;
@@ -28,18 +73,23 @@ const getDragaliaUnits = async () => {
             name: name,
             image: image,
             description: description,
+            cv: cv,
             title: title,
             rarity: rarity,
             element: element,
             weapon: weapon,
             role: role,
             atk: atk,
-            hp: hp
+            hp: hp,
+            abilities: abilities,
+            skills: skills
         };
-        console.log(adventurer);
+
+        if(name === "Marth"){console.dir(adventurer, {depth: null, colors: true})}
     });
 };
 
 getDragaliaUnits().then(() => console.log("Starting."));
+
 
 
