7c94e6a7698306186f162571ad37100fab490fdc
Anthony <lachhman.anthony@gmail.com>
2019-12-13 18:22:55 -0500

    Update Dragon Scraper and Made Dragon.js

--- src/scripts/dragscripts/getDragonData.js
+++ src/scripts/dragscripts/getDragonData.js
@@ -20,25 +20,57 @@ async function getDragaliaDragonData(links) {
         let html = await axios.get(link);
         const $ = cheerio.load(html.data);
         const name = $('h1').text();
-        console.log('writing ' + name);
         const isLocked = ((name.includes("(") && !name.includes("Dragon")) || name === "Rush");
         const image = $('#mw-content-text > div > div.panel > div.panel-body > div.portrait-container > div > div:nth-child(1) > p > a > img').attr("src");
         const str = $('#adv-str').text();
         const hp = $('#adv-hp').text();
         const maxMight = $('#mw-content-text > div > div.panel > div.panel-body > div:nth-child(2) > div:nth-child(5) > div:nth-child(2) > span').text().replace('Does not include external buffs (e.g. Halidom, Wyrmprints, etc.)Max HP + Max Str + Lv. 2 Skill Might + Lv. 2 Ability Might + Lv. 30 Bond (* Elemental Matching Bonus)', '');
-        const rarityAlt = (isLocked)?'5':$('#mw-content-text > div > div.panel > div.panel-body > div:nth-child(2) > div:nth-child(6) > div:nth-child(2) > div > img').attr('alt');
+        const rarityAlt = (isLocked) ? '5' : $('#mw-content-text > div > div.panel > div.panel-body > div:nth-child(2) > div:nth-child(6) > div:nth-child(2) > div > img').attr('alt');
         const rarity = rarityAlt.includes('5') ? 5 : (rarityAlt.includes('4') ? 4 : 3);
         const favoriteFood = $('#mw-content-text > div > div.panel > div.panel-body > div:nth-child(2) > div:nth-child(8) > div:nth-child(2) > span > a:nth-child(2)').text();
 
         const skillName = $('#mw-content-text > div > div.panel > div:nth-child(3) > div:nth-child(2) > table > tbody > tr > th > a').text();
         const skillLevels = [];
-        $('#mw-content-text > div > div.panel > div:nth-child(3) > div:nth-child(2) > table > tbody > tr > td > div > div').each((i, div)=>{
-            skillLevels.push($(div).text().split('\n')[2].substring(0,$(div).text().split('\n')[2].indexOf(' (Might')))
+        $('#mw-content-text > div > div.panel > div:nth-child(3) > div:nth-child(2) > table > tbody > tr > td > div > div').each((i, div) => {
+            skillLevels.push($(div).text().split('\n')[2].substring(0, $(div).text().split('\n')[2].indexOf(' (Might')))
         });
 
-        const abilityLevels = [];
-        $('.skill-section > .skill-table.skill-level').each((i, table)=>{
-            
+        const abilities = [];
+        $('#mw-content-text > div > div.panel > div:nth-child(3) > div.skill-section > table').each((i, table) => {
+            const abiName = $(table).find('tbody > tr > th > a').text();
+            const abiLevels = [];
+            $(table).find('tbody > tr > td > div > div').each((i, div) => {
+                abiLevels.push($(div).text().split('\n')[1].substring(0, $(div).text().split('\n')[1].indexOf(' (Might')).replace(/\t/g, ''))
+            });
+            abilities.push({
+                name: abiName,
+                levels: abiLevels
+            })
+        });
+
+        const comboTable = [];
+        $('.wikitable > tbody > tr').each((i, tr) => {
+            let multiplier = "";
+            let hitCount = "";
+            if (i !== 0) {
+                $(tr).find('td').each((i, td) => {
+                    switch (i) {
+                        case 0:
+                            break;
+                        case 1:
+                            multiplier = $(td).text();
+                            break;
+                        case 2:
+                            hitCount = $(td).text();
+                            break;
+                    }
+                });
+                comboTable.push({
+                    name: "Combo " + i,
+                    multiplier: multiplier,
+                    hitCount: hitCount.replace('\n', '')
+                });
+            }
         });
 
         const skill = {
@@ -54,9 +86,12 @@ async function getDragaliaDragonData(links) {
             maxMight: maxMight,
             rarity: rarity,
             favoriteFood: favoriteFood,
-            skill: skill
+            skill: skill,
+            abilities: abilities,
+            comboTable: comboTable
         };
-        console.log(dragon)
+        console.dir(dragon, {depth: 5, colors: true});
+        console.log(',');
     }
 }
 
@@ -74,6 +109,7 @@ const dragonPageLinks = [
     'https://dragalialost.gamepedia.com/Cerberus',
     'https://dragalialost.gamepedia.com/Chthonius',
     'https://dragalialost.gamepedia.com/Cinder_Drake',
+    'https://dragalialost.gamepedia.com/Corsaint_Phoenix',
     'https://dragalialost.gamepedia.com/Cupid',
     'https://dragalialost.gamepedia.com/Dragonyule_Jeanne',
     'https://dragalialost.gamepedia.com/Erasmus',
