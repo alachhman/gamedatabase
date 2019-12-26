c87bcc794a3da5b96ffcdba2d356fc49d271f5fa
Anthony <lachhman.anthony@gmail.com>
2019-12-26 11:08:58 -0500

    Update Weapon Scraper

--- src/scripts/dragscripts/getWeaponData.js
+++ src/scripts/dragscripts/getWeaponData.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 log = console.log;
 
-async function getDragaliaWeapons(){
+async function getDragaliaWeapons() {
     let json = await axios.get(
         "https://gamepress.gg/sites/default/files/aggregatedjson/DragaliaLostWeaponsList.json?10040605992016338578"
     );
@@ -14,6 +14,7 @@ async function getDragaliaWeapons(){
         );
         const $ = cheerio.load(html.data);
         const name = $('h1').text();
+        const image = $('#weapon-image > img').attr('src');
         const atk = weapon.max_atk;
         const hp = weapon.max_hp;
         const rarity = weapon.rarity_num;
@@ -21,50 +22,63 @@ async function getDragaliaWeapons(){
         const element = weapon.element;
         let skillName = "";
         let skillLevels = [];
-        let abilityName = [];
+        let abilities = [];
         let hasSkill = false;
         let hasAbility = false;
-        const h2 = $('h2').each((i, h2)=>{
-            if($(h2).text().includes('Skill')){
+        const h2 = $('h2').each((i, h2) => {
+            if ($(h2).text().includes('Skill')) {
                 hasSkill = true;
             }
-            if($(h2).text().includes('Abilities')){
+            if ($(h2).text().includes('Abilities')) {
                 hasAbility = true;
             }
         });
-        if(hasSkill){
-            $('div:nth-child(10).views-element-container > div > div > div > div > span > table > tbody > tr').each((i, tr)=>{
-                if(i === 0){
+        if (hasSkill) {
+            $('div:nth-child(10).views-element-container > div > div > div > div > span > table > tbody > tr').each((i, tr) => {
+                if (i === 0) {
                     skillName = $(tr).find('b > a').text();
-                }else{
-                    $(tr).find('td').each((i, tr)=>{
-                        if(i === 0){
+                } else {
+                    $(tr).find('td').each((i, tr) => {
+                        if (i === 0) {
                             skillLevels.push($(tr).text())
                         }
                     })
                 }
             });
-        }else{
+        } else {
             skillName = 'None';
-            skillLevels = ['None'];
+            skillLevels = ['None.'];
         }
-        if(hasAbility){
-
+        if (hasAbility) {
+            $('.ability-info').each((i, div) => {
+                abilities.push({
+                    name: $(div).find('b > a').text(),
+                    effect: $(div).find('p').text()
+                });
+            });
+        } else {
+            abilities.push({
+                name: 'None',
+                effect: 'None.'
+            });
         }
 
         const wep = {
             name: name,
+            image: image,
             atk: atk,
             hp: hp,
             rarity: rarity,
             type: type,
             element: element,
-            skill:{
+            skill: {
                 name: skillName,
                 levels: skillLevels
             },
+            abilities: abilities
         };
-        //console.dir(wep, {depth: 5, colors: true});
+        console.dir(wep, {depth: 5, colors: true});
+        console.log(',');
     });
 }
 
